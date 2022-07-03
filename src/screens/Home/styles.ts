f9e9de5bbeb4402b8ba64c5586264219966e1b6b
Douglas Soares <douglassoares16.dev@gmail.com>
2022-07-03 11:13:59 -0300

    feat: Created Header of Home Screen

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,32 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import theme from "../../styles/theme";
+
+const { colors, fonts } = theme;
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${colors.header};
+
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${fonts.primary_400};
+  color: ${colors.text};
+`;
