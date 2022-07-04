16bfe5ef9ebf5e45369faee26350a6324ffa3941
Douglas Soares <douglassoares16.dev@gmail.com>
2022-07-04 18:38:42 -0300

    feat: Created CarDetails Screen

---
+++ src/components/Accessory/styles.ts
@@ -0,0 +1,23 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  min-width: 26%;
+  flex: 1;
+
+  height: 92px;
+
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+
+  padding: 16px;
+  margin-right: 8px;
+  margin-bottom: 8px;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(13)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+import { Accessory } from "../../components/Accessory";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Brand,
+  Content,
+  Description,
+  Details,
+  Name,
+  Period,
+  Price,
+  Rent,
+  About,
+  Accessories
+} from "./styles";
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton />
+      </Header>
+
+      <CarImages>
+        <ImageSlider
+          imageUrl={["https://i.pinimg.com/originals/e3/99/6c/e3996cbc32b254dd28205dd7e36a6a11.png"]}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 500,00</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
+        <About>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias incidunt quia at quo porro commodi, libero dicta ipsum, dolorum sunt sint perspiciatis molestiae hic adipisci minima non impedit beatae nisi?
+        </About>
+      </Content>
+    </Container>
+  );
+}
