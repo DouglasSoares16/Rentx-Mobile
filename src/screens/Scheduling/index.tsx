57d5cba460479adce5fcc01a7d700234b804325a
Douglas Soares <douglassoares16.dev@gmail.com>
2022-07-05 11:29:37 -0300

    feat: Created Scheduling Screen

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useTheme } from "styled-components/native";
+
+import { BackButton } from "../../components/BackButton";
+
+import ArrowSvg from "../../assets/arrow.svg";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer
+} from "./styles";
+import { StatusBar } from "react-native";
+import { Button } from "../../components/Button";
+
+export function Scheduling() {
+  const { colors } = useTheme();
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Header>
+        <BackButton color={colors.shape} />
+
+        <Title>
+          Escolha uma {"\n"}
+          data de início e {"\n"}
+          fim do aluguel {"\n"}
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>05/07/2022</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>20/07/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
