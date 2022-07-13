9e50406c03d39cea6739f4d9322f40268415d4c8
Douglas Soares <douglassoares16.dev@gmail.com>
2022-07-13 08:14:09 -0300

    feat: Created SignUpSecondStep Screen

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,11 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Keyboard, ScrollView, TouchableWithoutFeedback } from "react-native";
+import { useTheme } from "styled-components/native";
 
 import { BackButton } from "../../../components/BackButton";
 import { Bullet } from "../../../components/Bullet";
 import { Button } from "../../../components/Button";
-import { Input } from "../../../components/Form/Input";
+import { PasswordInput } from "../../../components/Form/PasswordInput";
 
 import {
   Container,
@@ -19,18 +20,14 @@ import {
 
 interface NavigationProps {
   goBack(): void;
-  navigate(screen: string): void;
 }
 
-export function SignUpFirstStep() {
-  const { goBack, navigate } = useNavigation<NavigationProps>();
+export function SignUpSecondStep() {
+  const { goBack } = useNavigation<NavigationProps>();
+  const { colors } = useTheme();
 
   function handleGoBack() {
     goBack();
-  }
-
-  function handleNextStep() {
-    navigate("SignUpSecondStep");
   }
 
   return (
@@ -55,25 +52,13 @@ export function SignUpFirstStep() {
           </SubTitle>
 
           <Form>
-            <FormTitle>1. Dados</FormTitle>
+            <FormTitle>2. Senha</FormTitle>
 
-            <Input iconName="user" placeholder="Nome" />
-
-            <Input
-              iconName="mail"
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCorrect={false}
-              autoCapitalize="none"
-            />
-
-            <Input
-              iconName="credit-card"
-              placeholder="CNH"
-              keyboardType="numeric" />
+            <PasswordInput iconName="lock" placeholder="Senha" />
+            <PasswordInput iconName="lock" placeholder="Repetir Senha" />
           </Form>
 
-          <Button title="Próximo" onPress={handleNextStep} />
+          <Button title="Cadastrar" color={colors.success} />
         </ScrollView>
       </Container>
     </TouchableWithoutFeedback>
