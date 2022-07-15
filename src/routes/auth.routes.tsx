4d7dc62c42011ccc5b717991e1ba634998c0281f
Douglas Soares <douglassoares16.dev@gmail.com>
2022-07-15 15:27:45 -0300

    refactor(AuthRoutes): Removed Home Screen

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -16,11 +16,6 @@ export function AuthRoutes() {
     }}>
       <Screen name="Splash" component={Splash} />
 
-      <Screen
-        name="Home"
-        component={Home}
-        options={{ gestureEnabled: false }} />
-
       <Screen name="SignIn" component={SignIn} />
       <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
       <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
